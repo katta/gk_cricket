5c95fc7fec007269268022405e792b08071fc967
Srivatsa Katta <vatsa.katta@gmail.com>
2016-08-28 08:14:12 -0400

    Renmaed swap to rotate_strike

--- lib/team_innings.rb
+++ lib/team_innings.rb
@@ -26,11 +26,11 @@ class TeamInnings
     @score_card.record(@striker, @bowler, score, current_over, current_ball)
 
     if Over.last_ball?(current_ball)
-      swap(@striker, @runner) if !([:one,:three,:five].include? score)
+      rotate_strike(@striker, @runner) if !([:one,:three,:five].include? score)
       @runner = next_batsman if score == :out
     else
       if ([:one,:three,:five].include? score)
-        swap(@striker, @runner)
+        rotate_strike(@striker, @runner)
       elsif score == :out
         @striker = next_batsman
       end
@@ -39,7 +39,7 @@ class TeamInnings
 
   private
 
-  def swap(striker, runner)
+  def rotate_strike(striker, runner)
     @striker = runner
     @runner = striker
   end
